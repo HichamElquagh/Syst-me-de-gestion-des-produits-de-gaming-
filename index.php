4f64627e7824ff7cd3a541c5070d735b8717bb33
HichamElquagh <alquaghhicham1@gmail.com>
2022-11-11 18:09:37 +0100

    create login page

--- index.php
+++ index.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css\bootstrap.min.css">    
+    <link rel="stylesheet" href="css\bootstrap.min.css">
     <link rel="stylesheet" href="css\all.min.css">
-    <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-<body>
-  <div class="container">
-    <div class="row justify-content-center align-items-center">
-        <div class="col-md-5">
-            <div class="form-box">
-                <div class="form-title">
-                    <h2 class="fw-bold">Login</h2>
-                </div>
-                <form action="" method="post">
-                    <div class="form-floating mb-3">
-                        <input type="email" class=" form-control form-control-sm" placeholder="Email" id="floatingInput">
-                        <label for="floatingInput">Email</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control form-control-sm" placeholder="password" id="floatingPassword">
-                        <label for="floatingPassword">Password</label>
-                    </div>
-                    <div class="mt-3">
-                        <button class="btn-primary">Login</button>
-                    </div>
 
-                </form>
+<body>
+    <div class="container ">
+        <div class="row justify-content-center align-items-center inner-row">
+            <div class=" col-lg-6 col-md-9 col-sm-12 ">
+                <div class="form-box p-5  ">
+                    <div class="form-title">
+                        <h2 class="fw-bold">Login</h2>
+                    </div>
+                    <form action="" method="post">
+                        <div class="form-floating mb-3">
+                            <input type="email" class=" form-control form-control-sm" placeholder="Email"
+                                id="floatingInput">
+                            <label for="floatingInput">Email</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control form-control-sm" placeholder="password"
+                                id="floatingPassword">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="mt-3">
+                            <button class="btn primaryBg">Login</button>
+                        </div>
+                    </form>
+                    <div class="mt-3">
+                        <span>Don't have account?</span> <button class="p-0 border-0 bg-transparent primaryColor ">Sign
+                            up </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-  </div>
-   
 
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/all.min.js"></script>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/all.min.js"></script>
 </body>
+
 </html>
